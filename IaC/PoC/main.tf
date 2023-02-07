18bfb77948010089ecaecd4855d13ed224834505
GalindoOscar <96539122+GalindoOscar@users.noreply.github.com>
2023-02-06 19:28:15 -0600

    Update main.tf
    oscorp_w organization is added

--- IaC/PoC/main.tf
+++ IaC/PoC/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     # hostname     = "app.terraform.io"
-    organization = "personal-mobile"
+    organization = "oscorp_w"
     workspaces {
       name = "poc_env"
     }
